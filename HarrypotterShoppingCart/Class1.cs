54d0cbdfe941dba4f3bfc2d70d725b635a67b872
alex.wang51 <alex.wang51@TI03NB12>
2019-02-18 12:01:41 +0800

    New solutions

--- HarrypotterShoppingCart/Class1.cs
+++ HarrypotterShoppingCart/Class1.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Runtime.InteropServices;
+using System.Xml.Schema;
+
 
 namespace HarrypotterShoppingCart
 {
@@ -12,15 +15,11 @@ namespace HarrypotterShoppingCart
         public Hashtable HandleInput(List<string> input)
         {
             Hashtable books = new Hashtable();
-
             foreach (string book in input)
             {
                 if (!books.ContainsKey(book))
                 {
                     books.Add(book, input.Count(name => name == book));
-                }
-                else {
-                    books[book] = (int)books[book] + 1 ;
                 }
             }
             return books;
@@ -36,20 +35,55 @@ namespace HarrypotterShoppingCart
 
             return sortedOrder;
         }
-        public int ProcessOrder(List<int> order)
+        public double ProcessOrder(List<int> order)
         {
             int singleprice = 100;
-            List<int> number = new List<int>();
             List<double> discount = new List<double> { 0.75, 0.8, 0.9, 0.95, 1 };
             double total = 0;
             for (int i = 0; i < 4; i++)
             {
                 total += (order[i + 1] - order[i]) * (5-i) * (double)singleprice * discount[i];
             }
-
-            return 0;
+            return total;
         }
 
+        public double ProcessOrderWithFilter(List<int> order)
+        { 
+            double total = 0;
+            while (order.Count != 0)
+            {
+                order.RemoveAll(item => item == 0);
+                double discountValue = CalcDiscount(order);
+                int itemQuantity = order.Min();
+                double price = CalcPrice(rtemQuantity, discountValue);
+                total += price;
+                RemoveProcessedItem(ref order, itemQuantity);
+            }
+            
+
+           
+            return total;
+        }
+
+        public double CalcPrice(int quantity, double discount)
+        {
+            return quantity * 100 * discount;
+        }
+
+        public void RemoveProcessedItem(ref List<int> order , int Quantity)
+        {
+            order.RemoveAll(item => item == Quantity);
+        }
+        public double CalcDiscount(List<int> order)
+        {
+            List<double> discount = new List<double>{1, 0.95, 0.9, 0.8, 0.75};
+            return discount[order.Count()-1];
+        }
+
+        
+
+        
+
     }
-   
+
 }
--- HarrypotterShoppingCart/cartTest.cs
+++ HarrypotterShoppingCart/cartTest.cs
@@ -1,20 +1,70 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
 
 namespace HarrypotterShoppingCart
 {   
     [TestFixture]
     class CartTest
-    {   
-        [Test]
-        public void Test()
+    {
+        [Ignore("Unfinished")]
+        public void TestProcess()
         {
             var Cart = new Class1();
-            Cart.
+            List<int> TestCase = new List<int>() { 1, 1, 1, 1, 1 };
+            double actual = Cart.ProcessOrder(TestCase);
+            Assert.True(actual == (double)375);
+        }
+
+        [Ignore("complete")]
+        public void TestFilter()
+        {
+            var Cart = new Class1();
+            List<string> orders = new List<string>{"Ep1", "Ep2", "Ep3", "Ep4", "Ep5"};
+            Hashtable actual = Cart.HandleInput(orders);
+            Hashtable expected = new Hashtable{{"EP1",1}, { "EP2", 1 }, { "EP3", 1 }, { "EP4", 1 }, { "EP5", 1 }};
+            Assert.True(actual ==  expected);
+
+        }
+
+        [Test]
+        public void TestDiscount()
+        {
+            var Cart = new Class1();
+            List<int> orders = new List<int> { 1, 1, 1, 1, 1 };
+           
+            double actual = Cart.CalcDiscount(orders); ;
+            double expected = 0.75;
+            Assert.True(actual == expected);
+
+        }
+
+
+        [Test]
+        public void TestTotalPrice()
+        {
+            var Cart = new Class1();
+            List<int> orders = new List<int>{1,2,3,4,5};
+            double actual = Cart.ProcessOrderWithFilter(orders);
+            double expected = 375;
+            Assert.True(actual == expected);
+        }
+
+        [Test]
+        public void TestRemovedItem()
+        {
+            var Cart = new Class1();
+            List<int> orders = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> expected = new List<int> {1, 3, 4, 5};
+            Cart.RemoveProcessedItem(ref orders, 2);
+
+            Assert.True(orders == expected);
         }
     }
 }
